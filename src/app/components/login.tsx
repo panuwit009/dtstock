c65317d47e615dc58490c1e01773b4774482f7e1
PPJP246 <rockzx007@gmail.com>
2025-08-06 01:48:42 +0700

    updata

--- src/app/components/login.tsx
+++ src/app/components/login.tsx
@@ -4,32 +4,33 @@ import { setIslogin } from "../type";
 export default function Login ({setIslogin}: setIslogin) {
     return (
 <div className="bg-gradient-to-br from-sky-200 via-sky-300 to-sky-400 min-h-screen w-screen flex justify-center items-center">
-      <div className="w-[90%] max-w-md bg-white rounded-xl shadow-lg p-8">
+      <div className="w-[90%] max-w-md bg-white rounded-xl shadow-2xl p-10">
         <div className="flex flex-col items-center space-y-6">
-          <div className="text-[50px] text-black">รูปภาพ</div>
-            <div className="w-full space-y-4">
-                <div>
-                    <label className="block text-sm font-medium text-gray-700 mb-1">Gmail</label>
-                    <input
-                        type="text"
-                        className="w-full px-3 py-2 rounded-md border-2 border-gray-500 focus:border-blue-400 focus:ring-1 focus:ring-blue-200 outline-none transition"
-                    />
-                </div>
+            <div className="text-3xl font-bold text-sky-600">เข้าสู่ระบบ</div>
+                <div className="text-black">รูปภาพ</div>
+                <div className="w-full space-y-5">
+                    <div>
+                        <label className="text-sm font-medium text-gray-700 mb-1">Gmail</label>
+                        <input
+                            type="text"
+                            className="w-full px-3 py-2 rounded-md border-2 border-gray-500 focus:border-blue-400 focus:ring-1 focus:ring-blue-200 outline-none transition"
+                        />
+                    </div>
 
-                <div>
-                    <label className="block text-sm font-medium text-gray-700 mb-1">Password</label>
-                    <input
-                        type="password"
-                        className="w-full px-3 py-2 rounded-md border-2 border-gray-500 focus:border-blue-400 focus:ring-1 focus:ring-blue-200 outline-none transition"
-                    />
-                </div>
+                    <div>
+                        <label className="text-sm font-medium text-gray-700 mb-1">Password</label>
+                        <input
+                            type="password"
+                            className="w-full px-3 py-2 rounded-md border-2 border-gray-500 focus:border-blue-400 focus:ring-1 focus:ring-blue-200 outline-none transition"
+                        />
+                    </div>
 
-                <button
-                className="w-full bg-blue-500 text-white font-semibold py-2 rounded-md hover:bg-blue-600 transition active:scale-[0.98]"
-                >
-                Login
-                </button>
-            </div>
+                    <button
+                    className="w-full bg-sky-500 text-white font-bold py-3 rounded-md hover:bg-sky-600 transition active:scale-[0.98]"
+                    >
+                    Login
+                    </button>
+                </div>
         </div>
       </div>
     </div>
