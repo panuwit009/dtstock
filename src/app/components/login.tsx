4808e4763e079c9ed6a89f519a6c345a1a9480a2
panuwit009 <tinsologuitar@gmail.com>
2025-08-05 22:26:14 +0700

    prepare login component

---
+++ src/app/components/login.tsx
@@ -0,0 +1,9 @@
+export default function Login () {
+    return (
+        <div className="bg-sky-500 h-1000 w-1000">
+            <div className="bg-lime-500 h-500 w-500">
+                {/* ของมึง */}
+            </div>
+        </div>
+    );
+}
