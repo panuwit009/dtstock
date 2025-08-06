a7e6feaad72e927ab64056c0dee5b9f5c24180b9
panuwit009 <tinsologuitar@gmail.com>
2025-08-07 00:35:52 +0700

    toggleable stocktype select

--- src/app/home/components/scanbarcode.tsx
+++ src/app/home/components/scanbarcode.tsx
@@ -2,11 +2,18 @@
 import { useState } from "react";
 function AddOrDecreaseStock() {
     const [stocktype, setStocktype] = useState("");
+    function selectStocktype(st: string) {
+        if(stocktype === st) {
+            setStocktype("");
+        } else {
+            setStocktype(st)
+        }
+    }
     return (<>
-    <span onClick={()=>setStocktype("add")}
-    className={`${stocktype === "add" && "underline text-blue-600"} hover:cursor-pointer`}>เพิ่มสต๊อค</span> | <span
-    onClick={()=>setStocktype("dec")}
-    className={`${stocktype === "dec" && "underline text-blue-600"}  hover:cursor-pointer`}>ตัดสต๊อค</span>
+    <span onClick={()=> selectStocktype("add")}
+    className={`${stocktype === "add" && "underline text-blue-600"} select-none hover:cursor-pointer`}>เพิ่มสต๊อค</span> | <span
+    onClick={()=> selectStocktype("dec")}
+    className={`${stocktype === "dec" && "underline text-blue-600"}  select-none hover:cursor-pointer`}>ตัดสต๊อค</span>
     </>);
 }
 export default function Scanbarcode () {
