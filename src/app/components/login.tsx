4775f41c6aa833bfccb50b1ce891ddc97ef364e6
PPJP246 <rockzx007@gmail.com>
2025-08-06 13:40:52 +0700

    ui login v4

--- src/app/components/login.tsx
+++ src/app/components/login.tsx
@@ -3,24 +3,24 @@ import { setIslogin } from "../type";
 
 export default function Login({ setIslogin }: setIslogin) {
   return (
-    <div className="bg-gradient-to-br from-sky-300 via-sky-400 to-sky-500 min-h-screen w-screen flex justify-center items-center">
-      <div className="w-[90%] max-w-md bg-white rounded-2xl shadow-2xl p-8">
+    <div className="bg-gradient-to-br from-sky-200 via-sky-300 to-sky-100 min-h-screen w-screen flex justify-center items-center">
+      <div className="w-[90%] md:w-[22%] max-w-md bg-white rounded-2xl shadow-2xl p-8">
         <div className="flex flex-col items-center space-y-4">
-          <div className="text-3xl font-bold text-sky-600">เข้าสู่ระบบ</div>
-          <div className="text-gray-500 text-sm">กรุณาเข้าสู่ระบบด้วย Gmail และรหัสผ่าน</div>
+          {/* <div className="text-3xl font-bold text-sky-600">Login</div> */}
           <div className="w-24 h-24 bg-sky-100 rounded-full flex items-center justify-center text-sky-500 text-xl font-bold shadow-inner">
             LOGO
           </div>
+          <div className="text-gray-500 text-sm">กรุณาเข้าสู่ระบบด้วย Gmail และ รหัสผ่าน</div>
         </div>
 
-        <div className="mt-8 space-y-6">
+        <div className="mt-6 space-y-4">
             <div className="bg-white p-6 rounded-xl border border-gray-300 shadow-lg space-y-4">
                 <div>
                     <label className="text-sm font-medium text-gray-700">Gmail</label>
                     <input
                     type="email"
                     placeholder="example@gmail.com"
-                    className="placeholder-gray-400 placeholder:text-sm placeholder:italic mt-1 w-full px-3 py-2 rounded-md border border-gray-300 focus:border-sky-500 focus:ring-1 focus:ring-sky-200 outline-none transition"
+                    className="placeholder-gray-400 placeholder:text-sm placeholder:italic text-black mt-1 w-full px-3 py-2 rounded-md border border-gray-300 focus:border-sky-500 focus:ring-1 focus:ring-sky-200 outline-none transition"
                     />
                 </div>
 
@@ -34,15 +34,20 @@ export default function Login({ setIslogin }: setIslogin) {
                 </div>
             </div>
 
-          <button
-            onClick={() => {
-              alert("เข้าสู่ระบบสำเร็จ");
-              setIslogin(true);
-            }}
-            className="w-full bg-sky-500 text-white font-semibold py-2 rounded-md hover:bg-sky-600 transition active:scale-[0.98]"
+          <button onClick={() => {alert("เข้าสู่ระบบสำเร็จ"); setIslogin(true);}}
+            className="w-full bg-sky-500 text-white font-semibold py-2 rounded-md hover:bg-sky-600 transition active:scale-[0.98] shadow-2xl duration-300 hover:scale-95"
           >
-            เข้าสู่ระบบ
+            Login
           </button>
+
+          <button 
+            className="w-full bg-yellow-500 text-white font-semibold py-2 rounded-md hover:bg-yellow-600 transition active:scale-[0.98] shadow-2xl duration-300 hover:scale-95"
+          >
+            Register
+          </button>
+
+          <div className="text-gray-500 text-center">Forget your password ?</div>
+
         </div>
       </div>
     </div>
