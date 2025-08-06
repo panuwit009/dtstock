10e9ea0aaf381b32be9302e4f9c6f20539aa01ca
panuwit009 <tinsologuitar@gmail.com>
2025-08-06 15:22:45 +0700

    use useRouter next js

--- src/app/home/components/dashboard.tsx
+++ src/app/home/components/dashboard.tsx
@@ -1,14 +1,15 @@
 "use client";
-import { setIslogin } from "../type";
+import { useRouter } from "next/navigation";
 import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-export default function Dashboard ({setIslogin}: setIslogin) {
+export default function Dashboard () {
+    const router = useRouter();
     return (
         <>
         <FontAwesomeIcon icon={faCoffee} /> <FontAwesomeIcon icon={faCheckSquare} />
         <div>show dashboard</div>
-        <button onClick={() => {alert("ออกจากระบบสำเร็จ"); setIslogin(false);}}>Logout</button>
+        <button onClick={() => {alert("ออกจากระบบสำเร็จ"); router.push('/'); }}>Logout</button>
         </>
     );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,21 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from 'next/navigation';
 import "@fontsource/kanit"; // Defaults to weight 400
 import "@fontsource/kanit/400.css"; // Specify weight
 import "@fontsource/kanit/400-italic.css"; // Specify weight and style
 
 import Login from "./components/login";
-import Dashboard from "./components/dashboard";
 
 export default function Main() {
   const [isLogin, setIslogin] = useState<boolean>(false);
+  const router = useRouter();
 
-  if (isLogin) {
-    return <Dashboard setIslogin={setIslogin}/>;
-  } else {
-    return <Login setIslogin={setIslogin}/>;
-  }
+  useEffect(() => {
+    if (isLogin) {
+      router.push('/home');
+    }
+  }, [isLogin, router]);
+
+  return <Login setIslogin={setIslogin}/>;
 }
