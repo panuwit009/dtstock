67e15731874c59b6dbeef8745c98988d2ed97825
panuwit009 <tinsologuitar@gmail.com>
2025-08-06 16:19:32 +0700

    install and test @zxing/browser

--- src/app/home/components/dashboard.tsx
+++ src/app/home/components/dashboard.tsx
@@ -1,15 +1,26 @@
 "use client";
 import { useRouter } from "next/navigation";
-import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { BrowserQRCodeReader } from '@zxing/browser';
 
 export default function Dashboard () {
+    function scanBarcode () {
+        const codeReader = new BrowserQRCodeReader();
+        console.log(codeReader);
+        //alert("เปิดกล้อง");
+    }
     const router = useRouter();
     return (
         <>
         <FontAwesomeIcon icon={faCoffee} /> <FontAwesomeIcon icon={faCheckSquare} />
         <div>show dashboard</div>
-        <button onClick={() => {alert("ออกจากระบบสำเร็จ"); router.push('/'); }}>Logout</button>
+        <div className="hover:cursor-pointer" onClick={scanBarcode}>Scan Barcode</div>
+        <button className="hover:cursor-pointer"
+        onClick={() => {
+            alert("ออกจากระบบสำเร็จ"); router.push('/'); }
+            }
+        >Logout</button>
         </>
     );
 }
