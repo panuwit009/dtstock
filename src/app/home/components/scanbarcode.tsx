79982097d3fda59ec23fd17ca618b2af46fa6d1f
panuwit009 <tinsologuitar@gmail.com>
2025-08-07 00:24:06 +0700

    add setstocktype

--- src/app/home/components/scanbarcode.tsx
+++ src/app/home/components/scanbarcode.tsx
@@ -1,9 +1,20 @@
+"use client";
+import { useState } from "react";
+function AddOrDecreaseStock() {
+    const [stocktype, setStocktype] = useState("");
+    return (<>
+    <span onClick={()=>setStocktype("add")}
+    className={`${stocktype === "add" && "underline text-blue-600"} hover:cursor-pointer`}>เพิ่มสต๊อค</span> | <span
+    onClick={()=>setStocktype("dec")}
+    className={`${stocktype === "dec" && "underline text-blue-600"}  hover:cursor-pointer`}>ตัดสต๊อค</span>
+    </>);
+}
 export default function Scanbarcode () {
     return (
         <div className="flex justify-center border rounded-lg">
             <div className="grid grid-rows-12 h-50">
                 <div className="row-span-8 text-center">
-                    <p>สแกนบาร์โค๊ดเพื่อจัดการสต๊อค หรือ</p>
+                    <span>สแกนบาร์โค๊ดเพื่อ </span><AddOrDecreaseStock /> หรือ
                     <p className="underline text-blue-600 hover:cursor-pointer">คลิกที่นี่</p>
                     <p>เพื่อเพิ่มข้อมูลด้วยการพิมพ์</p>
                 </div>
