1a32b125fc8d414804b88aadab6b415aa99f2c7f
panuwit009 <tinsologuitar@gmail.com>
2025-08-06 13:31:04 +0700

    install kanit font and test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
 "use client";
 import { useState } from "react";
+import "@fontsource/kanit"; // Defaults to weight 400
+import "@fontsource/kanit/400.css"; // Specify weight
+import "@fontsource/kanit/400-italic.css"; // Specify weight and style
 
 import Login from "./components/login";
 import Dashboard from "./components/dashboard";
