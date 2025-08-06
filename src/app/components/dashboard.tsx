3d12f56743b181bc809cd1e145b9f98df8b05ca5
panuwit009 <tinsologuitar@gmail.com>
2025-08-06 13:17:56 +0700

    install and tested FontAwesomeIcon package

--- src/app/components/dashboard.tsx
+++ src/app/components/dashboard.tsx
@@ -1,8 +1,12 @@
 "use client";
 import { setIslogin } from "../type";
+import { faCheckSquare, faCoffee } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 export default function Dashboard ({setIslogin}: setIslogin) {
     return (
         <>
+        <FontAwesomeIcon icon={faCoffee} /> <FontAwesomeIcon icon={faCheckSquare} />
         <div>show dashboard</div>
         <button onClick={() => {alert("ออกจากระบบสำเร็จ"); setIslogin(false);}}>Logout</button>
         </>
