4258f3d6d4c2a97b00b8b724d92390ea02397828
PPJP246 <rockzx007@gmail.com>
2025-08-07 20:01:01 +0700

    ui login v.5.9

--- src/app/components/headlogin.tsx
+++ src/app/components/headlogin.tsx
@@ -1,10 +1,14 @@
 export default function HeadLogin () {
     return (
         <div className="flex flex-col items-center space-y-4">
-          <div className="text-3xl font-bold text-sky-600">DTstock Login</div>
-            <div className="w-24 h-24 rounded-full bg-gray-100 bg-cover bg-center drop-shadow-sm/30"
+            <div className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-sky-500 via-sky-600 to-sky-500">
+                DTstock Login
+            </div>
+
+             <div className="w-24 h-24 rounded-full bg-gray-100 bg-cover bg-center drop-shadow-sm/30"
                     style={{ backgroundImage: "url('/img/logo.webp')" }}>
             </div>
+            
             <div className="text-gray-600 text-sm">Use your Gmail and password to sign in.</div>
           </div>
     )
