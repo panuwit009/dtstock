b2e76ec59197eb1bf6f6df40f4b0332f62071c4a
partyend <sugar13055@gmail.com>
2025-08-07 23:41:13 +0700

    add |

--- src/app/components/headlogin.tsx
+++ src/app/components/headlogin.tsx
@@ -2,7 +2,7 @@ export default function HeadLogin () {
     return (
         <div className="flex flex-col items-center space-y-4">
             <div className="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-sky-500 via-sky-600 to-sky-500">
-                DTstock Login
+                DTstock | Login
             </div>
 
              <div className="w-24 h-24 rounded-full bg-gray-100 bg-cover bg-center drop-shadow-sm/30"
