5f0fbac74cb40c30a31858d1979460bf1d3ad6a4
partyend <sugar13055@gmail.com>
2025-08-09 16:14:47 +0700

    fix error with dynamix ssr

--- src/app/components/loading.tsx
+++ src/app/components/loading.tsx
@@ -1,23 +1,30 @@
 "use client";
 
-import { Player } from "@lottiefiles/react-lottie-player";
+import dynamic from "next/dynamic";
 import React from "react";
-import animationData from "../../../public/lottie/Loading-DTstock.json"
+import animationData from "../../../public/lottie/Loading-DTstock.json";
+
+const Player = dynamic(
+  () => import("@lottiefiles/react-lottie-player").then((mod) => mod.Player),
+  { ssr: false }
+);
 
 const LottieLoader = () => {
   return (
-    <div style={{
-      width: "100vw",
-      height: "100vh",
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      background: "#fff",
-      zIndex: 9999,
-      position: "fixed",
-      top: 0,
-      left: 0
-    }}>
+    <div
+      style={{
+        width: "100vw",
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        background: "#fff",
+        zIndex: 9999,
+        position: "fixed",
+        top: 0,
+        left: 0,
+      }}
+    >
       <Player
         autoplay
         loop
