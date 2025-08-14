887b2e51331f9b2d777e835a3b71dc2bcd1bf681
PPJP246 <rockzx007@gmail.com>
2025-08-14 20:43:53 +0700

    ui alert

--- src/app/utils/alert.tsx
+++ src/app/utils/alert.tsx
@@ -26,15 +26,16 @@ export default function AlertPTB ({type}: any) {
             <div className="fadeInUpOnce fixed bg-black/40 inset-0 flex justify-center items-center z-10" >
 
                 <div className="bg-white rounded-2xl w-sm relative flex flex-col items-center space-y-7 px-10 py-6 drop-shadow-lg/20 
-                                border-t-6 border-emerald-300">
+                                border-t-6 border-emerald-400">
 
                     <div className='text-black absolute top-2 right-3 rounded-full hover:cursor-pointer'
                     onClick={()=>setShow(false)}>
                         ✖
                      </div>
 
-                    <div className={`text-white text-3xl bg-gradient-to-br from-emerald-300 to-green-400 px-6 py-4 rounded-full animate-bounce`}>
+                    <div className={`text-white text-3xl bg-gradient-to-br from-emerald-400 to-green-400 px-6 py-4 rounded-full animate-bounce`}>
                         ✔
+                        
                     </div>
 
                     <div className='text-center space-y-3 hover:cursor-default'>
@@ -43,11 +44,12 @@ export default function AlertPTB ({type}: any) {
                         </div>
 
                         <div className="text-gray-500 text-sm">
-                            Welcome!
+                            Welcome !
                         </div>
                     </div>
 
-                    <div className="w-full text-center bg-gradient-to-br from-emerald-300 to-green-400 text-white rounded-full p-4 font-semibold hover:cursor-pointer"
+                    <div className="w-full text-center bg-gradient-to-br from-emerald-400 to-green-500 text-white rounded-2xl shadow-lg p-4 font-semibold 
+                                    translate duration-300 hover:cursor-pointer hover:scale-110 active:scale-95"
                     onClick={()=>setShow(false)}>
                        Confirm
                     </div>
