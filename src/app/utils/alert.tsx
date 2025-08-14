481e86447b0827b73452bd91dd855448b856eb4f
PPJP246 <rockzx007@gmail.com>
2025-08-14 14:13:12 +0700

    ui alert

--- src/app/utils/alert.tsx
+++ src/app/utils/alert.tsx
@@ -23,26 +23,34 @@ export default function AlertPTB () {
     if (show) {
         return (
 
-            <div className="fixed bg-black/40 inset-0 flex justify-center items-center z-10" >
+            <div className="fadeInUpOnce fixed bg-black/40 inset-0 flex justify-center items-center z-10" >
 
-                <div className="bg-white rounded-2xl w-sm relative flex flex-col items-center space-y-7 px-10 py-6 drop-shadow-lg/20 border-t-6 border-[#b7f473]">
+                <div className="bg-white rounded-2xl w-sm relative flex flex-col items-center space-y-7 px-10 py-6 drop-shadow-lg/20 
+                                border-t-6 border-emerald-300">
 
                     <div className='text-black absolute top-2 right-3 rounded-full hover:cursor-pointer'
                     onClick={()=>setShow(false)}>
                         ✖
                      </div>
 
-                    <div className={`text-white text-3xl bg-[#b7f473] px-6 py-4 rounded-full`}>
+                    <div className={`text-white text-3xl bg-gradient-to-br from-emerald-300 to-green-400 px-6 py-4 rounded-full shadow-lg animate-bounce 
+                                     hover:cursor-default`}>
                         ✔
                     </div>
 
-                    <div className="text-black text-xl font-semibold" id='test'>
-                        เข้าสู่ระบบสำเร็จ
+                    <div className='text-center space-y-3 hover:cursor-default'>
+                        <div className="text-black text-2xl font-semibold" id='test'>
+                            Login Successful
+                        </div>
+
+                        <div className="text-gray-500 text-sm">
+                            Welcome!
+                        </div>
                     </div>
 
-                    <div className="w-full text-center text-xl bg-[#b7f473] text-white rounded-full p-4 font-semibold hover:cursor-pointer"
+                    <div className="w-full text-center bg-gradient-to-br from-emerald-300 to-green-400 text-white rounded-full p-4 font-semibold hover:cursor-pointer"
                     onClick={()=>setShow(false)}>
-                       ยืนยัน
+                       Confirm
                     </div>
 
                 </div>
