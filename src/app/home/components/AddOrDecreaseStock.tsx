33fbc43e8009f1b5425c8c689060f37fe7c5ff9c
PPJP246 <rockzx007@gmail.com>
2025-08-15 21:03:35 +0700

    ui scanbarcode

--- src/app/home/components/AddOrDecreaseStock.tsx
+++ src/app/home/components/AddOrDecreaseStock.tsx
@@ -11,9 +11,50 @@ export function AddOrDecreaseStock() {
         }
     }
     return (<>
-    <span onClick={()=> selectStocktype("add")}
-    className={`${stocktype === "add" && "underline text-blue-600"} select-none hover:cursor-pointer`}>เพิ่มสต๊อค</span> | <span
-    onClick={()=> selectStocktype("dec")}
-    className={`${stocktype === "dec" && "underline text-blue-600"}  select-none hover:cursor-pointer`}>ตัดสต๊อค</span>
+                           <div className="flex items-center justify-center pt-8 space-x-8">
+
+                                <button
+                                    onClick={() => selectStocktype("add")}
+                                    className={`${stocktype === "add" ? "text-white bg-emerald-400 focus:scale-115" : "bg-white"} 
+                                        hover:scale-110 shadow-lg text-emerald-400 px-6 py-4 rounded-2xl font-medium transition duration-300 border-2 border-emerald-400 hover:cursor-pointer flex items-center gap-2`}
+                                    >
+                                    <svg
+                                        className="w-6 h-6"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M12 4v16m8-8H4"
+                                        />
+                                    </svg>
+                                    เพิ่มสต๊อค
+                                </button>
+
+                                <button
+                                    onClick={() => selectStocktype("dec")}
+                                    className={`${stocktype === "dec" ? "text-white bg-rose-600 focus:scale-115" : "bg-white"} 
+                                        hover:scale-110 shadow-lg text-rose-400 px-6 py-4 rounded-2xl font-medium transition duration-300 border-2 border-red-200 hover:cursor-pointer flex items-center gap-2`}
+                                    >
+                                    <svg
+                                        className="w-6 h-6"
+                                        fill="none"
+                                        stroke="currentColor"
+                                        viewBox="0 0 24 24"
+                                    >
+                                        <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth="2"
+                                        d="M20 12H4"
+                                        />
+                                    </svg>
+                                    ตัดสต๊อค
+                                </button>
+
+                            </div>
     </>);
 }
--- src/app/home/components/scanbarcode.tsx
+++ src/app/home/components/scanbarcode.tsx
@@ -8,23 +8,82 @@ export default function Scanbarcode () {
     const [error, setError] = useState<string | null>(null);
     const router = useRouter();
 
+        const [stocktype, setStocktype] = useState("");
+    function selectStocktype(st: string) {
+        if(stocktype === st) {
+            setStocktype("");
+        } else {
+            setStocktype(st)
+        }
+    }
+
     return (
-        <div className="flex justify-center rounded-lg">
-            <div className="grid grid-rows-12 h-50 border border-black w-[50%]">
-                <div className="row-span-8 text-center">
-                    <span>สแกนบาร์โค๊ดเพื่อ </span>
-                    <AddOrDecreaseStock /> หรือ
-                    <p className="underline text-blue-600 hover:cursor-pointer">คลิกที่นี่</p>
-                    <p>เพื่อเพิ่มข้อมูลด้วยการพิมพ์</p>
+        <div className="p-4">
+            
+                <div className="text-center m-4">
+
+                    <div className="w-16 h-16 bg-blue-500 rounded-2xl flex items-center justify-center mx-auto mb-4">
+                        <svg
+                        className="w-8 h-8 text-white"
+                        fill="none"
+                        stroke="currentColor"
+                        viewBox="0 0 24 24"
+                        >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M3 7l9-4 9 4-9 4-9-4z"
+                        />
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M3 7v10l9 4 9-4V7"
+                        />
+                        </svg>
+                    </div>
+   
+                    <h1 className="text-3xl font-bold text-gray-800">
+                        ระบบการจัดสินค้าสต็อค 
+                    </h1>
                 </div>
-                <div className="row-span-4 text-center">
-                    <p className="hover:cursor-pointer"
-                    onClick={() => openCamera({setError})}>ปุ่มเปิดกล้อง</p>
-                    <p className="hover:cursor-pointer"
-                    onClick={() => { alert("ออกจากระบบสำเร็จ"); router.push('/'); }}>Logout</p>
-                    {error && <p className="text-red-500 hover:cursor-pointer" onClick={() => setError("")}>{error}</p>}
+
+                <div className="bg-white rounded-3xl shadow-xl border border-gray-100 max-w-2xl mx-auto">
+
+                        <div className="space-y-8">
+                                <AddOrDecreaseStock />
+
+                        <div className="text-center">
+                            <p className="hover:cursor-pointer bg-stone-400 inline-block p-4 m-2 rounded-lg font-semibold"
+                            onClick={() => openCamera({setError})}>ปุ่มเปิดกล้อง</p>
+                            {error && <p className="text-red-500 hover:cursor-pointer" onClick={() => setError("")}>{error}</p>}
+                        </div>
+
+                        <div className="text-center">
+                            <div className="bg-white border-2 border-gray-200 inline-block p-6 space-y-2">
+
+                                    <div className="underline text-blue-600 hover:cursor-pointer">
+                                        คลิกที่นี่
+                                    </div>
+                                    
+                                    <div>
+                                        เพื่อเพิ่มข้อมูลด้วยการพิมพ์
+                                    </div>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div className="flex justify-end p-4">
+
+                                        <p className="hover:cursor-pointer"
+                            onClick={() => { alert("ออกจากระบบสำเร็จ"); router.push('/'); }}>Logout</p>
+                    </div>
+
+
                 </div>
-            </div>
         </div>
     );
 }
