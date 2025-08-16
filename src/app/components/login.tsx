037c33c9a2531c4ef75e0769fe9731f47913e77c
panuwit009 <tinsologuitar@gmail.com>
2025-08-16 16:58:15 +0700

    test call alert

--- src/app/components/login.tsx
+++ src/app/components/login.tsx
@@ -1,15 +1,14 @@
 "use client";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 
 import HeadLogin from "./headlogin";
 import Bodylogin from "./formlogin";
-import AlertPTB from "../utils/alert";
+import { useAlert } from "../utils/alertcontext";
 // import LottieLoader from "../utils/loading";
 import type { setIslogin } from "../type";
 
 export default function Login ({ setIslogin }: setIslogin) {
-    const [isLoading, setIsLoading] = useState(false);
-    const [popup, setPopup] = useState(false);
+    // const [isLoading, setIsLoading] = useState(false);
 
     // useEffect(() => {
     //   const timer = setTimeout(() => {
@@ -19,52 +18,41 @@ export default function Login ({ setIslogin }: setIslogin) {
     // }, []);
 
     //if (isLoading) return <LottieLoader />;
-
+    const { setShow } = useAlert();
     function clickLoginButton (): void {
-      setPopup(true);
-      setTimeout(() => {
+      setShow({ type: 'success' });
+      // setTimeout(() => {
         setIslogin(true);
-      }, 2000);
+      // }, 0);
     }
 
   return (
-    <>
-        {popup && <AlertPTB type="error" />}
+  <div className="min-h-screen w-screen flex justify-center items-center">
+    <div className="border-white/30 border w-[95%] md:w-[40%] max-w-md max-h-md bg-white/80 rounded-2xl shadow-2xl p-8">
+      <HeadLogin />
+      <Bodylogin />
+      <div className="mt-6 space-y-4">
+        <button onClick={clickLoginButton}
+        className="text-lg font-semibold w-full bg-sky-500 text-white p-2 rounded-lg shadow-lg hover:bg-sky-600 
+        transform transition active:scale-95
+        hover:scale-105 hover:cursor-pointer">
+          Login
+        </button>
         
-        <div className="min-h-screen w-screen flex justify-center items-center">
-          <div className="border-white/30 border w-[95%] md:w-[40%] max-w-md max-h-md bg-white/80 rounded-2xl shadow-2xl p-8">
-
-            <HeadLogin />
-              <Bodylogin />
-           
-            <div className="mt-6 space-y-4">
-
-              <button onClick={clickLoginButton}
-                  className="text-lg font-semibold w-full bg-sky-500 text-white p-2 rounded-lg shadow-lg hover:bg-sky-600 
-                  transform transition active:scale-95
-                  hover:scale-105 hover:cursor-pointer">
-                  Login
-              </button>
-
-              <button 
-                  className="text-lg font-semibold w-full bg-yellow-500 text-white p-2 rounded-lg shadow-lg hover:bg-yellow-600 
-                  transform transition active:scale-95
-                  hover:scale-105 hover:cursor-pointer">
-                  Register
-              </button>
-
-              <div 
-                  className="text-sky-600 text-center text-sm 
-                  hover:underline hover:cursor-pointer">
-                  Forget your password ?
-              </div>
-
-            </div>
-
+        <button 
+        className="text-lg font-semibold w-full bg-yellow-500 text-white p-2 rounded-lg shadow-lg hover:bg-yellow-600 
+        transform transition active:scale-95
+        hover:scale-105 hover:cursor-pointer">
+          Register
+        </button>
+        
+        <div 
+        className="text-sky-600 text-center text-sm 
+        hover:underline hover:cursor-pointer">
+          Forget your password ?
+        </div>
       </div>
     </div>
-
-    
-      </>
+  </div>
   );
 }
