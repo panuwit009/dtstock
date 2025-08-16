c644bacedf33fbe4bd1f5a108ecff5427d3c1563
panuwit009 <tinsologuitar@gmail.com>
2025-08-16 21:07:54 +0700

    write switch alert look

--- src/app/utils/alert.tsx
+++ src/app/utils/alert.tsx
@@ -4,40 +4,61 @@
 import { useAlert } from './alertcontext';
 
 export default function AlertPTB () {
-    const { show, setShow } = useAlert();
+    const { show, al } = useAlert();
+
+    let styles = {
+        color: '',
+        icon: ''
+    };
 
     if (show) {
+        switch (show.type) {
+            case 'success' :
+                styles.color = 'emerald-400';
+                styles.icon = '✔';
+                break;
+            case 'error' :
+                styles.color = 'red-400';
+                styles.icon = '✖';
+                break;
+            default :
+                styles.color = '';
+                styles.icon = '!';
+        }
         return (
 
             <div className="fadeInUpOnce fixed bg-black/40 inset-0 flex justify-center items-center z-10" >
 
-                <div className="bg-white rounded-2xl w-sm relative flex flex-col items-center space-y-7 px-10 py-6 drop-shadow-lg/20 
-                                border-t-6 border-emerald-400">
+                <div className={`bg-white rounded-2xl w-sm relative flex flex-col items-center space-y-7 px-10 py-6 drop-shadow-lg/20 
+                                border-t-6 border-${styles.color}`}>
 
                     <div className='text-black absolute top-2 right-3 rounded-full hover:cursor-pointer'
-                    onClick={()=>setShow(null)}>
+                    onClick={()=>al(null)}>
                         ✖
                      </div>
 
-                    <div className={`text-white text-3xl bg-gradient-to-br from-emerald-400 to-green-400 px-6 py-4 rounded-full animate-bounce`}>
-                        ✔
+                    <div className={`text-white text-3xl bg-gradient-to-br from-bg-emerald-400 to-green-400 px-6 py-4 rounded-full animate-bounce`}>
+                        {styles.icon}
                     </div>
 
                     <div className='text-center space-y-3 hover:cursor-default'>
+                        {show.headerMessage &&
                         <div className="text-black text-2xl font-semibold">
-                            Login Successful
+                            {show.headerMessage}
                         </div>
+                        }
 
                         <div className="text-gray-500 text-sm">
-                            Welcome ! <br /> {show?.type && `ทดสอบ alert type: ${show?.type}`}
+                            {show.message && <>{show.message}<br /></>}
                         </div>
                     </div>
 
+                    {show.confirmBt && 
                     <div className="w-full text-center bg-gradient-to-br from-emerald-400 to-green-500 text-white rounded-2xl shadow-lg p-4 font-semibold 
                                     translate duration-300 hover:cursor-pointer hover:scale-110 active:scale-95"
-                    onClick={()=>setShow(null)}>
-                       Confirm
-                    </div>
+                    onClick={()=>al(null)}>
+                       {show.confirmBtText ? show.confirmBtText : 'Confirm'}
+                    </div>}
 
                 </div>
             </div>
