acbfb93a6757a7787bdd1feb404c40cb70575ae6
panuwit009 <tinsologuitar@gmail.com>
2025-08-16 17:01:12 +0700

    refactor from switch case to useState to update alert content

--- src/app/utils/alert.tsx
+++ src/app/utils/alert.tsx
@@ -1,24 +1,15 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons'
-import { useState } from 'react';
-import "./alert.css";
+"use client";
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faCheck, faXmark } from '@fortawesome/free-solid-svg-icons';
+import { useAlert } from './alertcontext';
 
-export default function AlertPTB ({type}: {type: string}) {
-    const [show, setShow] = useState(true);
-    let message = "ทดสอบ";
+export function runAlert() {
+  const { setShow } = useAlert();
+  setShow({ type: 'success' });
+}
 
-    switch (type) {
-        case "success":
-            message = "✔";
-            break;
-        case "error":
-            message = "✖";
-            break;
-        default :
-            message = "Error";
-        break;
-
-    }
+export default function AlertPTB () {
+    const { show, setShow } = useAlert();
 
     if (show) {
         return (
@@ -29,28 +20,27 @@ export default function AlertPTB ({type}: {type: string}) {
                                 border-t-6 border-emerald-400">
 
                     <div className='text-black absolute top-2 right-3 rounded-full hover:cursor-pointer'
-                    onClick={()=>setShow(false)}>
+                    onClick={()=>setShow(null)}>
                         ✖
                      </div>
 
                     <div className={`text-white text-3xl bg-gradient-to-br from-emerald-400 to-green-400 px-6 py-4 rounded-full animate-bounce`}>
                         ✔
-                        
                     </div>
 
                     <div className='text-center space-y-3 hover:cursor-default'>
-                        <div className="text-black text-2xl font-semibold" id='test'>
+                        <div className="text-black text-2xl font-semibold">
                             Login Successful
                         </div>
 
                         <div className="text-gray-500 text-sm">
-                            Welcome !
+                            Welcome ! <br /> {show?.type && `ทดสอบ alert type: ${show?.type}`}
                         </div>
                     </div>
 
                     <div className="w-full text-center bg-gradient-to-br from-emerald-400 to-green-500 text-white rounded-2xl shadow-lg p-4 font-semibold 
                                     translate duration-300 hover:cursor-pointer hover:scale-110 active:scale-95"
-                    onClick={()=>setShow(false)}>
+                    onClick={()=>setShow(null)}>
                        Confirm
                     </div>
 
