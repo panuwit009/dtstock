89960b998344c9282cc705c3558c2df38bcbde8c
panuwit009 <tinsologuitar@gmail.com>
2025-08-23 18:14:52 +0700

    add mockup data

--- src/app/home/components/mockupdata.ts
+++ src/app/home/components/mockupdata.ts
@@ -13,5 +13,7 @@ export const mockupdata: Item[] = [
     ,{ id: 4, name: "ยาพารา", amount: 20, image:"https://get.pxhere.com/photo/tree-horizon-road-field-prairie-highway-driving-asphalt-dirt-road-open-road-lane-plain-road-trip-grassland-infrastructure-rural-area-road-surface-nonbuilding-structure-controlled-access-highway-1456.jpg" }
     ,{ id: 5, name: "ยาย่าฉันไม่ใช่เทวดา", amount: 60, image:"https://cms.dmpcdn.com/musicarticle/2019/02/21/3fc84571-0385-4ee7-adfd-aac80b799175.jpg" }
     ,{ id: 6, name: "ยาดม", amount: 40, image:"https://www.mocagrimart.com/th/file/get/file/20230430d0164eceeb84beb9f4898f11d6a4ba5b132241.jpg" }
+    ,{ id: 7, name: "โทรไปไม่รับ You miss call, Oh I really miss you", amount: 59, image:"https://is1-ssl.mzstatic.com/image/thumb/Music211/v4/21/06/3a/21063af3-af02-c1c1-3f9b-39c23218944c/0.jpg/592x592bb.webp" }
+    
 
 ];
