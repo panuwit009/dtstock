1e9f908af457fd82a7cc5ea97f124757eaa82e11
panuwit009 <tinsologuitar@gmail.com>
2025-09-03 16:25:50 +0700

    change switch case condition

--- src/app/home/components/cardsSwitchPercentColor.ts
+++ src/app/home/components/cardsSwitchPercentColor.ts
@@ -1,24 +1,12 @@
 export const cardsSwitchPercentColor = (amountPercent: number) => {
     switch (true) {
-        case amountPercent <= 10 :
+        case amountPercent <= 25 :
             return "border-red-600";
-        case amountPercent > 10 && amountPercent <= 20 :
-            return "border-red-600";
-        case amountPercent > 20 && amountPercent <= 30 :
+        case amountPercent > 25 && amountPercent <= 50 :
             return "border-orange-400";
-        case amountPercent > 30 && amountPercent <= 40 :
-            return "border-orange-400";
-        case amountPercent > 40 && amountPercent <= 50 :
+        case amountPercent > 50 && amountPercent <= 75 :
             return "border-yellow-400";
-        case amountPercent > 50 && amountPercent <= 60 :
-            return "border-yellow-400";
-        case amountPercent > 60 && amountPercent <= 70 :
-            return "border-green-400";
-        case amountPercent > 70 && amountPercent <= 80 :
-            return "border-green-400";
-        case amountPercent > 80 && amountPercent <= 90 :
-            return "border-green-400";
-        case amountPercent > 90 :
+        case amountPercent > 75 :
             return "border-green-400";
             default:
                 return null;
