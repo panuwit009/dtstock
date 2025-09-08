a3a737eb097d4fd7f8684b27aa5014d41589c7db
panuwit009 <tinsologuitar@gmail.com>
2025-09-08 11:51:41 +0700

    create pre-load function

--- src/app/components/formlogin.tsx
+++ src/app/components/formlogin.tsx
@@ -1,44 +1,61 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { faKey, faUser, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
-
 export default function Bodylogin () {
-    const [showPassword, setShowPassword] = useState(false);
+  const [FontAwesomeLoading, setFontAwesomeLoading] = useState(true);
+  useEffect(() => {
+    const checkFontAwesome = () => {
+      if (faKey && faUser && faEye && faEyeSlash) {
+        setFontAwesomeLoading(false);
+      }
+    };
+    
+    checkFontAwesome();
+  }, []);
 
-        function Open () {
-      setShowPassword(prev=>!prev);
-    }
-
-    return(
-                    <div className="mt-6 space-y-4">
-                        <div className="transition duration-300 focus-within:scale-105">
-                            <label className="font-medium text-gray-700">Gmail</label>
-                          <div className="relative">
-                            <FontAwesomeIcon icon={faUser} className="text-gray-700 text-lg absolute left-3 top-1/2 -translate-y-1/2 rounded-md"/>
-                            <input
-                            type="email"
-                            placeholder="example@gmail.com"
-                            className="placeholder-gray-400 placeholder:italic text-black mt-1 w-full pl-10 pr-4 py-2 rounded-md border border-gray-400 focus:border-sky-500 focus:ring-1 focus:ring-sky-200 outline-none"
-                            />
-                            </div>
-                        </div>
-
-                        <div className="transition duration-300 focus-within:scale-105">
-                          <label className="font-medium text-gray-700">Password</label>
-                          <div className="relative">
-                            <FontAwesomeIcon icon={faKey} className="text-gray-700 text-lg absolute left-3 top-1/2 -translate-y-1/2 rounded-md"/>
-                            <input
-                            type={showPassword ? "text" : "password" }
-                            placeholder="12345678910"
-                            className="placeholder-gray-400 placeholder:italic text-black mt-1 w-full pl-10 pr-10 py-2 rounded-md border border-gray-400 focus:border-sky-500 focus:ring-1 focus:ring-sky-200 outline-none"
-                            />
-                            <button className="text-gray-600 text-md absolute right-3 top-1/2 -translate-y-1/2 rounded-md hover:cursor-pointer hover:text-sky-600" 
-                            onClick={Open}> 
-                            <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash}/>
-                             </button>
-                          </div>
-                       </div>
-                    </div>
-    )
+  const [showPassword, setShowPassword] = useState(false);
+  
+  function Open () {
+    setShowPassword(prev=>!prev);
+  }
+  
+  return(
+  <div className="mt-6 space-y-4">
+    <div className="transition duration-300 focus-within:scale-105">
+      <label className="font-medium text-gray-700">Gmail</label>
+      <div className="relative">
+        {!FontAwesomeLoading &&
+        <FontAwesomeIcon icon={faUser} className="text-gray-700 text-lg absolute left-3 top-1/2 -translate-y-1/2 rounded-md"/>
+        }
+        <input
+        type="email"
+        placeholder="example@gmail.com"
+        className="placeholder-gray-400 placeholder:italic text-black mt-1 w-full pl-10 pr-4 py-2 rounded-md border border-gray-400 focus:border-sky-500 focus:ring-1 focus:ring-sky-200 outline-none"
+        />
+      </div>
+    </div>
+    
+    <div className="transition duration-300 focus-within:scale-105">
+      <label className="font-medium text-gray-700">Password</label>
+      <div className="relative">
+        {!FontAwesomeLoading &&
+        <FontAwesomeIcon icon={faKey} className="text-gray-700 text-lg absolute left-3 top-1/2 -translate-y-1/2 rounded-md"/>
+        }
+        <input
+        type={showPassword ? "text" : "password" }
+        placeholder="12345678910"
+        className="placeholder-gray-400 placeholder:italic text-black mt-1 w-full pl-10 pr-10 py-2 rounded-md border border-gray-400 focus:border-sky-500 focus:ring-1 focus:ring-sky-200 outline-none"
+        />
+        
+        <button className="text-gray-600 text-md absolute right-3 top-1/2 -translate-y-1/2 rounded-md hover:cursor-pointer hover:text-sky-600" 
+        onClick={Open}>
+          {!FontAwesomeLoading &&
+          <FontAwesomeIcon icon={showPassword ? faEye : faEyeSlash}/>
+          }
+        </button>
+      </div>
+    </div>
+  </div>
+  )
 }
--- src/app/components/login.tsx
+++ src/app/components/login.tsx
@@ -1,10 +1,6 @@
 "use client";
-// import { useEffect, useState } from "react";
-
 import HeadLogin from "./headlogin";
 import Bodylogin from "./formlogin";
-import { useShow } from "../utils/showcontext";
-// import LottieLoader from "../utils/loading";
 import type { setIslogin } from "../type";
 import { useState } from "react";
 import './loading.css';
@@ -12,76 +8,47 @@ import './loading.css';
 export default function Login ({ setIslogin }: setIslogin) {
 const [isLoading, setIsLoading] = useState(false);
 
-const handleChanage = () => {
-    setIsLoading(true);
+const clickLoginButton = () => {
+  setIsLoading(true); setIslogin(true);
+  // setTimeout( ()=> setIslogin(true), 1000);
+};
 
-    setTimeout(() => {
-      setIsLoading(false);
-      setIslogin(true)
-    }, 2000);
-  };
-
-    // const [isLoading, setIsLoading] = useState(false);
-
-    // useEffect(() => {
-    //   const timer = setTimeout(() => {
-    //     setIsLoading(false);
-    //   }, 5000);
-    //   return () => clearTimeout(timer);
-    // }, []);
-
-    //if (isLoading) return <LottieLoader />;
-    const { setShow } = useShow();
-    function clickLoginButton (): void {
-      setShow(
-        { type: 'alert',
-          detail:
-          { status: 'success',
-            headerMessage: 'เข้าสู่ระบบสำเร็จ'
+return (
+<div className="min-h-screen w-screen flex justify-center items-center">
+  <div className="border-white/30 border w-[95%] md:w-[40%] max-w-md max-h-md bg-white/80 rounded-2xl shadow-2xl p-8">
+    <HeadLogin />
+    <Bodylogin />
+    <div className="mt-6 space-y-4">
+      <button 
+        onClick={clickLoginButton}
+        className={`flex justify-center items-center h-12 text-lg font-semibold w-full text-white p-2 rounded-lg shadow-lg
+          transform transition active:scale-95
+          ${isLoading
+            ? 'bg-sky-600 cursor-not-allowed'
+            : 'bg-sky-400 cursor-pointer'
           }
-        });
-      // setTimeout(() => {
-        setIslogin(true);
-      // }, 0);
-    }
-
-  return (
-          <div className="min-h-screen w-screen flex justify-center items-center">
-            <div className="border-white/30 border w-[95%] md:w-[40%] max-w-md max-h-md bg-white/80 rounded-2xl shadow-2xl p-8">
-              <HeadLogin />
-              <Bodylogin />
-              <div className="mt-6 space-y-4">
-                <button 
-                        onClick={handleChanage}
-                        className={`flex justify-center items-center h-12 text-lg font-semibold w-full text-white p-2 rounded-lg shadow-lg
-                        transform transition active:scale-95
-                ${isLoading
-                  ? 'bg-sky-600 cursor-not-allowed'
-                  : 'bg-sky-400'
-                }
-                  `}>
-                    {isLoading 
-                      ? 
-                        <div className="w-6 h-6 border-[5px] inline-block border-white border-b-transparent rounded-full box-border animate-spin"></div>
-                      : 'Login'
-                    }
-                    
-                </button>
-                
-                <button 
-                className="text-lg font-semibold w-full bg-yellow-500 text-white p-2 rounded-lg shadow-lg hover:bg-yellow-600 
-                transform transition active:scale-95
-                hover:scale-105 hover:cursor-pointer">
-                  Register
-                </button>
-                
-                <div 
-                className="text-sky-600 text-center text-sm 
-                hover:underline hover:cursor-pointer">
-                  Forget your password ?
-                </div>
-              </div>
-            </div>
+        `}>
+          {isLoading 
+          ? 
+          <div className="w-6 h-6 border-[5px] inline-block border-white border-b-transparent rounded-full box-border animate-spin"></div>
+          : 'Login'
+          }
+      </button>
+      
+      <button 
+      className="text-lg font-semibold w-full bg-yellow-500 text-white p-2 rounded-lg shadow-lg hover:bg-yellow-600 
+      transform transition active:scale-95
+      hover:scale-105 hover:cursor-pointer">
+        Register
+      </button>
+      
+      <div 
+      className="text-sky-600 text-center text-sm 
+      hover:underline hover:cursor-pointer">
+        Forget your password ?
       </div>
-  );
+    </div>
+  </div>
+</div>
+);
 }
