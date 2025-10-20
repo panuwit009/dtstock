b9a229e6f64e99a759e0361a54dfa43f11343515
panuwit009 <tinsologuitar@gmail.com>
2025-10-20 14:56:21 +0700

    .

--- src/app/test/alert/page.tsx
+++ src/app/test/alert/page.tsx
@@ -7,29 +7,14 @@ import {
     AlertPTBConfirmButton
 } from "@/app/utils/alert";
 
+
 export default function TestAlert () {
     const { setShow } = useShow();
-
+    
     return (
     <>
     <div className="h-screen flex items-center justify-center">
-        <button onClick={() =>
-            setShow 
-            (
-            <AlertPTB>
-                <AlertPTBIcon className = "bg-gradient-to-br from-emerald-400 to-green-400">
-                    ✔
-                </AlertPTBIcon>
-                <AlertPTBMessageSection>
-                    <AlertPTBHeaderMessage>ข้อความอะไรสักอย่าง</AlertPTBHeaderMessage>
-                    <AlertPTBMessage>ข้อความอะไรสักอย่างเหมือนกัน</AlertPTBMessage>
-                </AlertPTBMessageSection>
-                <AlertPTBConfirmButton className = "bg-gradient-to-br from-emerald-400 to-green-500">
-                    ตกลง
-                </AlertPTBConfirmButton>
-            </AlertPTB>
-            )
-        }
+        <button onClick={() => setShow (cusAlert)}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             Test Show
         </button>
@@ -37,3 +22,17 @@ export default function TestAlert () {
     </>
     )
 }
+
+const cusAlert =
+    <AlertPTB>
+        <AlertPTBIcon className = "bg-gradient-to-br from-emerald-400 to-green-400">
+            ✔
+        </AlertPTBIcon>
+        <AlertPTBMessageSection>
+            <AlertPTBHeaderMessage>ข้อความอะไรสักอย่าง</AlertPTBHeaderMessage>
+            <AlertPTBMessage>ข้อความอะไรสักอย่างเหมือนกัน</AlertPTBMessage>
+        </AlertPTBMessageSection>
+        <AlertPTBConfirmButton className = "bg-gradient-to-br from-emerald-400 to-green-500">
+            ตกลง
+        </AlertPTBConfirmButton>
+    </AlertPTB>;
