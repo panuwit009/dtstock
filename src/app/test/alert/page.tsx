196905e26bb2ea968e70f104b1847bc89af6ad09
panuwit009 <tinsologuitar@gmail.com>
2025-10-20 14:51:41 +0700

    use React node in alert

--- src/app/test/alert/page.tsx
+++ src/app/test/alert/page.tsx
@@ -1,6 +1,11 @@
 "use client";
 import { useShow } from "@/app/utils/showcontext";
-import AlertPTB from "@/app/utils/alert";
+import { 
+    AlertPTB,
+    AlertPTBIcon,
+    AlertPTBMessageSection, AlertPTBHeaderMessage, AlertPTBMessage,
+    AlertPTBConfirmButton
+} from "@/app/utils/alert";
 
 export default function TestAlert () {
     const { setShow } = useShow();
@@ -8,7 +13,23 @@ export default function TestAlert () {
     return (
     <>
     <div className="h-screen flex items-center justify-center">
-        <button onClick={() => setShow(<AlertPTB />)}
+        <button onClick={() =>
+            setShow 
+            (
+            <AlertPTB>
+                <AlertPTBIcon className = "bg-gradient-to-br from-emerald-400 to-green-400">
+                    ✔
+                </AlertPTBIcon>
+                <AlertPTBMessageSection>
+                    <AlertPTBHeaderMessage>ข้อความอะไรสักอย่าง</AlertPTBHeaderMessage>
+                    <AlertPTBMessage>ข้อความอะไรสักอย่างเหมือนกัน</AlertPTBMessage>
+                </AlertPTBMessageSection>
+                <AlertPTBConfirmButton className = "bg-gradient-to-br from-emerald-400 to-green-500">
+                    ตกลง
+                </AlertPTBConfirmButton>
+            </AlertPTB>
+            )
+        }
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             Test Show
         </button>
--- src/app/utils/alert.tsx
+++ src/app/utils/alert.tsx
@@ -1,60 +1,66 @@
 import { useShow } from './showcontext';
 
-export default function AlertPTB () {
+export function AlertPTB ( { children }: { children: React.ReactNode; } ) {
     const { setShow } = useShow();
 
-    // const styles = {
-    //     color: '',
-    //     icon: ''
-    // };
-
-        // switch (show.detail.status) {
-        //     case 'success' :
-        //         styles.color = 'emerald-400';
-        //         styles.icon = '✔';
-        //         break;
-        //     case 'error' :
-        //         styles.color = 'red-400';
-        //         styles.icon = '✖';
-        //         break;
-        //     default :
-        //         styles.color = '';
-        //         styles.icon = '!';
-        // }
-        return (
-
-            <div className="fixed bg-black/40 inset-0 flex justify-center items-center z-10" onClick={()=>setShow(null)}>
-
-                <div className={`bg-white rounded-2xl w-sm relative flex flex-col items-center space-y-7 px-10 py-6 drop-shadow-lg/20 
-                                border-t-6 border-emerald-400`}>
-
-                    <div className='text-black absolute top-2 right-3 rounded-full hover:cursor-pointer'
-                    onClick={()=>setShow(null)}>
-                        ✖
-                     </div>
-
-                    <div className={`text-white text-3xl bg-gradient-to-br from-emerald-400 to-green-400 px-6 py-4 rounded-full animate-bounce`}>
-                        ✔
-                    </div>
-
-                    <div className='text-center space-y-3 hover:cursor-default'>
-                        <div className="text-black text-2xl font-semibold">
-                            headerMessage
-                        </div>
-
-                        <div className="text-gray-500 text-sm">
-                            <>show.detail.message<br /></>
-                        </div>
-                    </div>
-
-                    <div className="w-full text-center bg-gradient-to-br from-emerald-400 to-green-500 text-white rounded-2xl shadow-lg p-4 font-semibold 
-                                    translate duration-300 hover:cursor-pointer hover:scale-110 active:scale-95"
-                    onClick={()=>setShow(null)}>
-                       Confirm
-                    </div>
-
-                </div>
-            </div>
-        );
-    }
+    const overlay = <div className="fixed bg-black/40 inset-0 z-10" onClick={()=>setShow(null)} />;
     
+    return (
+        <>
+            {overlay}
+
+            <div className={`fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2  z-11
+            bg-white rounded-2xl w-sm flex flex-col items-center space-y-7 px-10 py-6 drop-shadow-lg/20 
+            border-t-6 border-emerald-400`}>   
+                <div className='text-black absolute top-2 right-3 rounded-full hover:cursor-pointer'
+                onClick={()=>setShow(null)}>
+                    ✖
+                </div>
+                { children }
+            </div>
+        </>
+    );
+}
+
+export function AlertPTBIcon ( { children, className }: { children: React.ReactNode; className?: string; } ) {
+    return (
+    <div className={`text-white text-3xl px-6 py-4 rounded-full animate-bounce ${className}`}>
+        { children }
+    </div>
+    );
+}
+
+export function AlertPTBMessageSection ( { children }: { children: React.ReactNode; } ) {
+    return (
+    <div className='text-center space-y-3 hover:cursor-default'>
+        { children }
+    </div>
+    );
+}
+
+export function AlertPTBHeaderMessage ( { children }: { children: React.ReactNode; } ) {
+    return (
+    <div className="text-black text-2xl font-semibold">
+        { children }
+    </div>
+    );
+}
+
+export function AlertPTBMessage ( { children }: { children: React.ReactNode; } ) {
+    return (
+    <div className="text-gray-500 text-sm">
+        { children }
+    </div>
+    );
+}
+
+export function AlertPTBConfirmButton ( { children, className }: { children: React.ReactNode; className?: string; } ) {
+    const { setShow } = useShow();
+    return (
+    <div className={`w-full text-center text-white rounded-2xl shadow-lg p-4 font-semibold
+        translate duration-300 hover:cursor-pointer hover:scale-110 active:scale-95 ${className}`}
+        onClick={()=>setShow(null)}>
+        { children }
+    </div>
+    );
+}
