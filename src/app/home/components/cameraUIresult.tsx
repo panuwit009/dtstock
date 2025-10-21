914cae949b8163e4780dd146b89db73da62c88e2
panuwit009 <tinsologuitar@gmail.com>
2025-10-21 14:48:10 +0700

    test camera

--- src/app/home/components/cameraUIresult.tsx
+++ src/app/home/components/cameraUIresult.tsx
@@ -3,8 +3,8 @@ import type { CameraResult } from "@/app/type";
 export default function CameraUiResultButton ({cameraResult}: {cameraResult: CameraResult}) {
     return (
         <button onClick={()=>console.log(cameraResult)}
-        className="absolute buttom-3 right-3 mr-3 px-3 py-1
-        bg-lime-600 text-white rounded-full shadow hover:bg-red-700">
+        className="absolute bottom-3 right-3 mr-3 px-3 py-1
+        bg-lime-600 text-white rounded-full shadow hover:bg-lime-700">
             {cameraResult.length}
         </button>
     );
--- src/app/home/components/cameraUi.tsx
+++ src/app/home/components/cameraUi.tsx
@@ -14,12 +14,28 @@ export default function CameraUi (
         closeCamera();
     }
     return (
-    <div className="relative w-full">
+    <>
+
         {/* id ของ video ต้องเท่ากับ video เพราะในฟังก์ชันเปิดกล้องน่าจะ getelementbyid*/}
-        <video id="video" className="w-full"/>
+        <video
+            id="video"
+            autoPlay
+            muted
+            playsInline
+            className="
+            absolute inset-0
+            object-cover
+            w-full h-full
+            [@media(orientation:portrait)]:rotate-90
+            [@media(orientation:portrait)]:w-screen
+            [@media(orientation:portrait)]:h-screen
+            "
+        />
+        
         {/* เส้นแนวนอน */}
         <div onClick={()=>openCamera({setCameraResult})}
-        className="absolute top-1/2 left-0 w-full h-[2px] bg-red-500"></div>
+        className="absolute top-1/2 left-0 w-full h-[2px] bg-red-500"/>
+
         <button onClick={stopCamera}
         className="absolute top-3 right-3 mr-3 px-3 py-1
         bg-red-600 text-white rounded-full shadow hover:bg-red-700">
@@ -27,13 +43,13 @@ export default function CameraUi (
         </button>
         
         <CameraUiResultButton cameraResult={cameraResult}/>
-        <div> {/* ทดสอบ เอาผลลัพธ์มาแสดงให้ดู */}
+        <div>
             <pre>{JSON.stringify(cameraResult, null, 2)}</pre>
         </div>
 
         
         {/* เส้นแนวตั้ง */}
         {/* <div className="absolute left-1/2 top-0 h-full w-[2px] bg-red-500"></div> */}
-    </div>
+    </>
     );
 }
