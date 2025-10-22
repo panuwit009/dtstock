b0762109b7b25e5b8562f741342f9441968d6376
panuwit009 <tinsologuitar@gmail.com>
2025-10-22 17:23:55 +0700

    เทสกล้อง

--- src/app/home/components/cameraUi.tsx
+++ src/app/home/components/cameraUi.tsx
@@ -16,10 +16,10 @@ export default function CameraUi (
     return (
     <div className="relative w-full">
         {/* id ของ video ต้องเท่ากับ video เพราะในฟังก์ชันเปิดกล้องน่าจะ getelementbyid*/}
-        <video id="video" className="w-full"/>
+        <video id="video" className="w-full max-h-[100vh] object-cover"/>
         {/* เส้นแนวนอน */}
         <div onClick={()=>openCamera({setCameraResult})}
-        className="absolute top-1/2 left-0 w-full h-[2px] bg-red-500"></div>
+        className="absolute top-1/2 left-0 w-full h-[2px] bg-red-500" />
         <button onClick={stopCamera}
         className="absolute top-3 right-3 mr-3 px-3 py-1
         bg-red-600 text-white rounded-full shadow hover:bg-red-700">
