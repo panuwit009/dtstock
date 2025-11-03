0b5aa6061c772bd9c4eb841c4f6c2d05ca44fa8c
ppp <rockzx007@gmail.com>
2025-11-03 20:58:24 +0700

    แก้ไข ui scanbarcode

--- src/app/home/components/Headscan.tsx
+++ src/app/home/components/Headscan.tsx
@@ -1,23 +1,19 @@
 export default function Headscan () {
     return (
-                <div className="text-center py-6">
-
-                    {/* <div className="w-20 h-20 bg-gradient-to-br from-blue-500 to-indigo-600 rounded-3xl flex items-center justify-center mx-auto mb-4">
-                        <svg className="w-10 h-10 text-white drop-shadow-lg" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"/>
-                        </svg>
-                    </div> */}
-   
-                    <div className="space-y-2">
-                        <div className="text-4xl font-bold text-gray-800">
-                            ระบบการจัดสินค้าสต็อค 
-                        </div>
-
-                        <div className="text-sm text-gray-700">
-                            การจัดการสินค้าอย่างมืออาชีพ 
-                        </div>
-                    </div>
-
-                </div>
+         <div className="text-center py-6">
+             {/* <div className="w-20 h-20 bg-gradient-to-br from-blue-500 to-indigo-600 rounded-3xl flex items-center justify-center mx-auto mb-4">
+                 <svg className="w-10 h-10 text-white drop-shadow-lg" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4"/>
+                 </svg>
+             </div> */}
+             <div className="space-y-2">
+                 <div className="text-4xl font-bold text-gray-800">
+                     ระบบการจัดสินค้าสต็อค 
+                 </div>
+                 <div className="text-sm text-gray-700">
+                      การจัดการสินค้าอย่างมืออาชีพ 
+                 </div>
+             </div>
+         </div>
     )
 }
--- src/app/home/components/scanbarcode.tsx
+++ src/app/home/components/scanbarcode.tsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { LoadingAnimation, logoutSuccess } from "@/app/components";
 import { useShow } from "@/app/utils"
-import Headscan  from "./Headscan";
+// import Headscan  from "./Headscan";
 import FormInsertItem from "././forminsertitem";
 
 export default function Scanbarcode (
@@ -21,89 +21,127 @@ export default function Scanbarcode (
 
     return (
     <>
-        <Headscan />
-        <div className="bg-white md:rounded-3xl shadow-xl border border-gray-100 max-w-3xl mx-auto">
-            <div className="">
-                <LoadingAnimation />
-                {/* <AddOrDecreaseStock 
-                stocktype={stocktype}
-                setStocktype={setStocktype}
-                /> */}
-                <div className="flex flex-col items-center gap-2">                  
-                    {error && <div className="text-red-500 hover:cursor-pointer" onClick={() => setError("")}>{error}</div>}
-                </div>
+        <Header>
+            <HeaderTitle>ระบบการจัดสินค้าสต็อค</HeaderTitle>
+            <HeaderSubtitle>การจัดการสินค้าอย่างมืออาชีพ</HeaderSubtitle>
+        </Header>
+        
+        <Body>
+               <LoadingAnimation/>
+               {/* <AddOrDecreaseStock 
+               stocktype={stocktype}
+               setStocktype={setStocktype}
+               /> */}
+               <div className="flex flex-col items-center gap-2">                  
+                   {error && <div className="text-red-500 hover:cursor-pointer" onClick={() => setError("")}>{error}</div>}
+               </div>
+               
+               <Message>หากเครื่องสแกน barcode ใช้งานไม่ได้</Message>
+               
+            <Scanbarcode>
+                <Uicamera>
+                    <Messagecamera>เปิดกล้อง</Messagecamera>
+                </Uicamera>
                 
-                <div className="flex items-center justify-center gap-4 px-6">
-                    <hr className="flex-1 border-gray-400"></hr>
-                    <div>หากเครื่องสแกน barcode ใช้งานไม่ได้</div>
-                    <hr className="flex-1 border-gray-400"></hr>
-                </div>
-                
-                <div className="flex items-center justify-center mt-4">
-                    <div className="border-2 border-gray-300 rounded-lg p-6 space-y-2 mr-2 hover:border-sky-400">
-                        <div className="flex items-center gap-4">
-                            <div className="bg-gradient-to-br from-blue-500 to-indigo-600 p-4 rounded-2xl">
-                                <svg className="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" >
-                                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 20h9M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4 12.5-12.5z"/>
-                                </svg>
-                            </div>
-                            
-                            <div className="flex flex-col items-center">
-                                <div 
-                                    className="underline font-bold text-blue-600 hover:cursor-pointer"
-                                    onClick={()=> setShow(<FormInsertItem />)}
-                                >
-                                    เพิ่มข้อมูลด้วยการพิมพ์
-                                </div>
-                                
-                                <div className="text-gray-500 text-sm">
-                                    กรอกข้อมูลด้วยตนเอง
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-
-                    <span>หรือ</span>
-                    
-                    <div
-                    className="flex items-center gap-2 text-white shadow-lg hover:cursor-pointer hover:scale-110 transition duration-300 bg-slate-600 p-6 m-2 rounded-xl"
-                    onClick={() => { setOpenCamera(true); }}>
-                        <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7h2l2-3h10l2 3h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1z"/> 
-                            <circle cx="12" cy="13" r="4" stroke="currentColor" strokeWidth="2" />
-                        </svg>
-                        เปิดกล้อง
-                    </div>
-                </div>
-                
-                <div className="grid grid-cols-2 px-4 p-4">
-                    <div className="hover:cursor-default flex items-center gap-1">
-                        <div className="">
-                            <svg className="w-6 h-6 text-emerald-500" fill="none" stroke="currentColor" viewBox="0 0 24 24" aria-label="system-ready">
-                                <circle cx="12" cy="12" r="9" strokeWidth="2" />
-                                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4" />
+                <div className="w-full p-2">
+                    <div className="flex items-center justify-between">
+                        <div className="bg-gradient-to-br from-blue-500 to-indigo-600 p-4 rounded-2xl shadow-lg hover:scale-110 transition-transform cursor-pointer">
+                             <svg className="w-5 h-5 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 20h9M16.5 3.5a2.121 2.121 0 0 1 3 3L7 19l-4 1 1-4 12.5-12.5z"/>
                             </svg>
-                        </div>
-                        
-                        <div className="text-sm text-emerald-500">
-                            ระบบพร้อมใช้งาน
-                        </div>
-                    </div>
-                    
-                    <div className="flex items-center justify-end gap-1">
-                        <svg className="w-6 h-6 text-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" aria-label="logout" >
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 5H7a2 2 0 00-2 2v10a2 2 0 002 2h6" />
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M17 16l4-4m0 0l-4-4m4 4H9" />
-                        </svg>
-                        
-                        <div className="hover:cursor-pointer gap-1 text-red-600 text-sm rounded-lg"
-                        onClick={logout}>
-                            ออกจากระบบ
-                        </div>
-                    </div>
+                         </div>
+
+                     </div>
                 </div>
-            </div>
-        </div>
+                 
+                  {/* <div
+                     className="flex items-center gap-2 text-white shadow-lg hover:cursor-pointer hover:scale-110 transition duration-300 bg-slate-600 p-6 m-2 rounded-xl"
+                     onClick={() => { setOpenCamera(true); }}>
+                         <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 7h2l2-3h10l2 3h2a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V8a1 1 0 0 1 1-1z"/> 
+                             <circle cx="12" cy="13" r="4" stroke="currentColor" strokeWidth="2" />
+                         </svg>
+                         เปิดกล้อง
+                 </div> */}
+             </Scanbarcode>        
+         </Body>
+
     </>
     );
+
+function Message ( { children }: { children: React.ReactNode;}) {
+    return (
+        <div className="flex items-center justify-center gap-4 px-6">
+             <hr className="flex-1 border-gray-400"></hr>
+             { children }
+             <hr className="flex-1 border-gray-400"></hr>
+        </div>
+    );
+}
+
+function Uicamera ( { children }: { children?: React.ReactNode; } ) {
+    return (
+        <div className="absolute -top-16 left-1/2 -translate-x-1/2 w-32 h-32 bg-white rounded-full shadow-lg flex items-center justify-center"
+              onClick={() => { setOpenCamera(true); }}>
+              <div className="flex flex-col items-center gap-2">
+                  <svg className="border-b-4 border-blue-200 w-14 h-14 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 9a2 2 0 012-2h.93a2 2 0 001.664-.89l.812-1.22A2 2 0 0110.07 4h3.86a2 2 0 011.664.89l.812 1.22A2 2 0 0018.07 7H19a2 2 0 012 2v9a2 2 0 01-2 2H5a2 2 0 01-2-2V9z"/>
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 13a3 3 0 11-6 0 3 3 0 016 0z"/>
+                  </svg>
+                  { children } 
+              </div>
+        </div>
+    );
+}
+
+function Messagecamera ( { children }: { children?: React.ReactNode; } ) {
+    return (
+                  <span className="text-sm text-gray-700"> { children } </span>
+    );
+}
+
+
+function Header ({children}: {children: React.ReactNode}) {
+    return (
+        <div className="text-center py-6">
+            <div className="space-y-2">
+                {children}
+            </div>
+        </div>
+    )
+}
+
+function HeaderTitle ({children}: {children: React.ReactNode}) {
+    return (
+        <div className="text-4xl font-bold text-gray-800">
+            {children}
+        </div>
+    )
+}
+
+function HeaderSubtitle ({children}: {children: React.ReactNode}) {
+    return (
+        <div className="text-sm text-gray-700">
+            {children}
+        </div>
+    )
+}
+
+function Body ({children}: {children: React.ReactNode}) {
+    return (
+        <div className="bg-sky-200/30 md:rounded-3xl shadow-xl border border-gray-100 max-w-lg mx-auto">
+            {children}
+        </div>
+    )
+}
+
+function Scanbarcode ({children}: {children: React.ReactNode}) {
+    return (
+        <div className="relative h-50 w-[90%] bg-white mx-auto rounded-3xl mt-22 mb-6 flex items-end">
+            {children}
+        </div>
+    )
+}
+
+
 }
