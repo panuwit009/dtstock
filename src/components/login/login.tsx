b0c8889de01cedbf9b8be0fb6bf1db6fdb4fd401
panuwit009 <tinsologuitar@gmail.com>
2025-11-06 12:51:41 +0700

    แก้ขนาด login container

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -4,10 +4,10 @@ import { Form } from "./components/form";
 export default function Login ({ setIslogin }: { setIslogin: React.Dispatch<React.SetStateAction<boolean>>}) {
   return (
     <div
-      className="min-h-dvh w-dvw flex justify-center items-center overflow-x-hidden overflow-y-auto
+      className="min-h-dvh max-w-dvw flex justify-center items-center
       bg-gradient-to-br from-sky-300 via-sky-200 to-white px-4"
     >
-      <main className="border-white/30 border w-sm md:w-md bg-white/80 rounded-2xl shadow-2xl pt-8 px-8 pb-4 xl:pb-8 my-auto">
+      <main className="border-white/30 border w-sm md:w-md bg-white/80 rounded-2xl shadow-2xl pt-8 pb-4 xl:pb-8 px-8 m-8">
         <Header />
         <Form setIslogin={setIslogin} />        
       </main>
