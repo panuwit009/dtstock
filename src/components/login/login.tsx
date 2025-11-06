1fa62a451f3553bad14abf6353e3e75c00c375d8
panuwit009 <tinsologuitar@gmail.com>
2025-11-06 12:56:51 +0700

    แก้ขนาด margin และ padding

--- src/components/login/login.tsx
+++ src/components/login/login.tsx
@@ -7,7 +7,7 @@ export default function Login ({ setIslogin }: { setIslogin: React.Dispatch<Reac
       className="min-h-dvh max-w-dvw flex justify-center items-center
       bg-gradient-to-br from-sky-300 via-sky-200 to-white px-4"
     >
-      <main className="border-white/30 border w-sm md:w-md bg-white/80 rounded-2xl shadow-2xl pt-8 pb-4 xl:pb-8 px-8 m-8">
+      <main className="border-white/30 border w-sm md:w-md bg-white/80 rounded-2xl shadow-2xl pt-8 pb-4 xl:pb-8 px-8 my-8">
         <Header />
         <Form setIslogin={setIslogin} />        
       </main>
