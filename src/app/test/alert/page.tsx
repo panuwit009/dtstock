3be00469ab3622fbcfcc85a0dedf3b4c2fab3d16
panuwit009 <tinsologuitar@gmail.com>
2025-11-07 16:42:15 +0700

    refactor sidebar ย้ายตัวแปร useState และลองนำไปใช้ที่อื่น

--- src/app/test/alert/page.tsx
+++ src/app/test/alert/page.tsx
@@ -1,25 +1,16 @@
 "use client";
-import { useRouter } from "next/navigation";
 import { example } from "@/components";
 import { useShow } from "@/utils";
 
 export default function TestAlert () {
     const { setShow } = useShow();
-    const router = useRouter();
 
     return (
-    <>
     <div className="h-screen flex items-center justify-center gap-4">
-        <button onClick={() => router.push("/home")}
-        className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-            Home
-        </button>
-
         <button onClick={() => setShow(example)}
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
             Test Show
         </button>
     </div>
-    </>
     )
 }
