d4fbbda7799e802f6a50b0af029648931baf89e7
panuwit009 <tinsologuitar@gmail.com>
2025-11-09 15:29:35 +0700

    เปลี่ยน%

--- src/components/card/function/switchcolorbypercent.ts
+++ src/components/card/function/switchcolorbypercent.ts
@@ -1,12 +1,12 @@
 export const switchcolorbypercent = (amountPercent: number) => {
     switch (true) {
-        case amountPercent <= 25 :
+        case amountPercent <= 16.68 :
             return "border-red-600";
-        case amountPercent > 25 && amountPercent <= 50 :
+        case amountPercent > 16.68 && amountPercent <= 33.4 :
             return "border-orange-400";
-        case amountPercent > 50 && amountPercent <= 75 :
+        case amountPercent > 33.4 && amountPercent <= 50 :
             return "border-yellow-400";
-        case amountPercent > 75 :
+        case amountPercent > 50 :
             return "border-green-400";
             default:
                 return null;
