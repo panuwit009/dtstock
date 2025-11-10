7d309caab7a5cfa85587eb913836d84c83283fd4
ppp <rockzx007@gmail.com>
2025-11-10 18:00:25 +0700

    ใส่ hover เพิ่มนิดหน่อย

--- src/components/home/header/index.tsx
+++ src/components/home/header/index.tsx
@@ -2,7 +2,7 @@ export default function HomeHeader () {
     return (
         <header className="row-span-3 px-4 flex items-center justify-between border-b border-b-gray-400">
             <div className="">
-                <span className="font-bold text-sm xl:text-2xl 2xl:text-3xl">Test</span>
+                <span className="font-bold text-sm xl:text-2xl 2xl:text-4xl">Dashboard</span>
                 <div className="text-lg text-gray-700 font-thin">การจัดการสินค้าอย่างมืออาชีพ</div>
             </div>
 
@@ -37,8 +37,8 @@ export default function HomeHeader () {
                     </div>
 
                     <div className="flex items-center justify-center">
-                        <div className="grid grid-cols-2 gap-9">
-                            <span className="relative w-16 h-16 rounded-full flex items-center justify-center bg-[#685762] hover:scale-110 hover:cursor-pointer hover:bg-blue-200 transition duration-150">
+                        <div className="grid grid-cols-2 gap-6">
+                            <span className="relative size-16 rounded-full flex items-center justify-center bg-[#685762] hover:scale-110 hover:cursor-pointer transition duration-150">
                                     <svg className="w-8 h-8" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                                         <path d="M12 2C11.4477 2 11 2.44772 11 3V3.5C8.51472 4.08996 6.5 6.24021 6.5 8.92308V12.5385C6.5 13.3946 6.21563 14.2154 5.7019 14.8462L4.61803 16.1538C3.87071 17.0769 4.52364 18.5 5.69231 18.5H18.3077C19.4764 18.5 20.1293 17.0769 19.382 16.1538L18.2981 14.8462C17.7844 14.2154 17.5 13.3946 17.5 12.5385V8.92308C17.5 6.24021 15.4853 4.08996 13 3.5V3C13 2.44772 12.5523 2 12 2Z" 
                                         fill="#ffffffff"/>
@@ -50,7 +50,7 @@ export default function HomeHeader () {
                             </span>
 
 
-                            <div className="w-16 h-16 bg-gray-100 rounded-full flex items-center justify-center hover:scale-110 hover:cursor-pointer hover:bg-blue-200 transition duration-150">
+                            <div className="size-16 bg-gray-100 rounded-full flex items-center justify-center hover:scale-110 hover:cursor-pointer hover:bg-blue-200 transition duration-150">
                                 <div className="w-full h-full bg-cover rounded-full bg-[url('/img/1.jpg')]">
                                 </div>
                             </div>
