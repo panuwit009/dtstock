4dae6eaca5f76bfa1a4b17d92bb0cf458afa86a1
panuwit009 <tinsologuitar@gmail.com>
2025-11-13 19:38:55 +0700

    ทำ responsive switch component

---
+++ src/components/responsiveswitch/index.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { useCheckScreen } from "@/utils";
+
+interface Props {
+  mobile?: React.ReactNode;
+  tablet?: React.ReactNode;
+  desktop?: React.ReactNode;
+}
+
+export function ResponsiveSwitch({ mobile, tablet, desktop }: Props) {
+  const { isTablet, isDesktop } = useCheckScreen();
+  
+  if (isTablet && tablet) {
+    return <>{tablet}</>;
+    } else if (isDesktop && desktop) {
+        return <>{desktop}</>;
+    }
+    return <>{mobile}</>;
+}
