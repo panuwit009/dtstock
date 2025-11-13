7ca68c445653e4afd57abbf3e2203079896f16d0
panuwit009 <tinsologuitar@gmail.com>
2025-11-13 19:40:05 +0700

    แก้ hook ให้ทำงานแค่เช็คจอ

--- src/utils/hooks/mediaquery/index.ts
+++ src/utils/hooks/mediaquery/index.ts
@@ -1,24 +1,18 @@
 "use client";
 import { useMediaQuery } from "react-responsive";
 
-export function useCheckScreen () {
-  const isMobile = useMediaQuery({ query: "(max-width: 640px)" });
-  const isTablet = useMediaQuery({ query: "(max-width: 1024px)" });
-  const isDesktop = useMediaQuery({ query: "(min-width: 1025px)" });
+export function useCheckScreen() {
+  // ตาม device
+  const isMobile = useMediaQuery({ query: "(max-width: 639px)" });
+  const isTablet = useMediaQuery({ query: "(min-width: 640px)" });
+  const isDesktop = useMediaQuery({ query: "(min-width: 1024px)" });
 
-  // Tailwind
+  // breakpoints ของ Tailwind
   const isSm = useMediaQuery({ query: "(min-width: 640px)" });
   const isMd = useMediaQuery({ query: "(min-width: 768px)" });
   const isLg = useMediaQuery({ query: "(min-width: 1024px)" });
   const isXl = useMediaQuery({ query: "(min-width: 1280px)" });
   const is2Xl = useMediaQuery({ query: "(min-width: 1536px)" });
 
-  let current = "ต่ำกว่า sm";
-  if (is2Xl) current = "2xl ขึ้นไป";
-  else if (isXl) current = "xl";
-  else if (isLg) current = "lg";
-  else if (isMd) current = "md";
-  else if (isSm) current = "sm";
-
-  return { isMobile, isTablet, isDesktop, isSm, isMd, isLg, isXl, is2Xl, current };
+  return { isMobile, isTablet, isDesktop, isSm, isMd, isLg, isXl, is2Xl };
 }
