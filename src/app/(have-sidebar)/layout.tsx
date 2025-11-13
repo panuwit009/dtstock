dce5fda2eac8cc58c5b2e63a85b0c3d3452bb469
panuwit009 <tinsologuitar@gmail.com>
2025-11-13 19:36:37 +0700

    responsive switch conditional

--- src/app/(have-sidebar)/layout.tsx
+++ src/app/(have-sidebar)/layout.tsx
@@ -1,22 +1,43 @@
 "use client";
 import { usePathname } from "next/navigation";
 import { useState, useEffect } from "react";
-import { Sidebar, SillyLoading } from "@/components";
+import { Sidebar, SillyLoading, ResponsiveSwitch } from "@/components";
 export default function Layout ({ children }: { children: React.ReactNode; }) {
+    return (
+        <ResponsiveSwitch 
+            mobile={<MobileView children={children}/>}
+            tablet={<TabletView children={children}/>}
+            desktop={<TabletView children={children}/>}
+        />
+    );
+}
+
+export function TabletView ({ children }: { children: React.ReactNode; }) {
     const [sidebarOpen, setSidebarOpen] = useState<boolean>(true);
     const [loading, setLoading] = useState<boolean>(true);
     const pathname = usePathname();
     useEffect(() => {
         setLoading(false);
     }, [pathname]);
-
     return (
         <div className="flex w-dvw h-dvh overflow-y-hidden">
             <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} setLoading={setLoading} loading={loading}/>
             <main 
-                className={`flex-1 flex-1 ${sidebarOpen ? "md:ml-64" : "md:ml-3"}`}
+                className={`flex-1 ${sidebarOpen ? "sm:ml-64" : "sm:ml-3"}`}
             >
                 { loading ? <SillyLoading /> : children }
+            </main>
+        </div>
+    );
+}
+
+export function MobileView ({ children }: { children: React.ReactNode; }) {
+    return (
+        <div className="flex w-dvw h-dvh overflow-y-hidden">
+            <main 
+                className={`flex-1`}
+            >
+                { children }
             </main>
         </div>
     );
