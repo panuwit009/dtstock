554df676822be45efa097b97f0f48e4977f21e4f
panuwit009 <tinsologuitar@gmail.com>
2025-11-14 13:24:15 +0700

    แก้ error react บอกห้ามส่ง children เป็น prop

--- src/app/(have-sidebar)/layout.tsx
+++ src/app/(have-sidebar)/layout.tsx
@@ -5,40 +5,40 @@ import { Sidebar, SillyLoading, ResponsiveSwitch } from "@/components";
 export default function Layout ({ children }: { children: React.ReactNode; }) {
     return (
         <ResponsiveSwitch 
-            mobile={<MobileView children={children}/>}
-            tablet={<TabletView children={children}/>}
-            desktop={<TabletView children={children}/>}
+            mobile={<MobileView />}
+            tablet={<TabletView />}
+            desktop={<TabletView />}
         />
     );
-}
 
-export function TabletView ({ children }: { children: React.ReactNode; }) {
-    const [sidebarOpen, setSidebarOpen] = useState<boolean>(true);
-    const [loading, setLoading] = useState<boolean>(true);
-    const pathname = usePathname();
-    useEffect(() => {
-        setLoading(false);
-    }, [pathname]);
-    return (
-        <div className="flex w-dvw h-dvh overflow-y-hidden">
-            <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} setLoading={setLoading} loading={loading}/>
-            <main 
-                className={`flex-1 ${sidebarOpen ? "sm:ml-64" : "sm:ml-3"}`}
-            >
-                { loading ? <SillyLoading /> : children }
-            </main>
-        </div>
-    );
-}
+    function TabletView () {
+        const [sidebarOpen, setSidebarOpen] = useState<boolean>(true);
+        const [loading, setLoading] = useState<boolean>(true);
+        const pathname = usePathname();
+        useEffect(() => {
+            setLoading(false);
+        }, [pathname]);
+        return (
+            <div className="flex w-dvw h-dvh overflow-y-hidden">
+                <Sidebar sidebarOpen={sidebarOpen} setSidebarOpen={setSidebarOpen} setLoading={setLoading} loading={loading}/>
+                <main 
+                    className={`flex-1 ${sidebarOpen ? "sm:ml-64" : "sm:ml-3"}`}
+                >
+                    { loading ? <SillyLoading /> : children }
+                </main>
+            </div>
+        );
+    }
 
-export function MobileView ({ children }: { children: React.ReactNode; }) {
-    return (
-        <div className="flex w-dvw h-dvh overflow-y-hidden">
-            <main 
-                className={`flex-1`}
-            >
-                { children }
-            </main>
-        </div>
-    );
+    function MobileView () {
+        return (
+            <div className="flex w-dvw h-dvh overflow-y-hidden">
+                <main 
+                    className={`flex-1`}
+                >
+                    { children }
+                </main>
+            </div>
+        );
+    }
 }
