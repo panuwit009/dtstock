36fbd77f8c4f78d9cd239967dd582809397674f3
ppp <rockzx007@gmail.com>
2025-11-22 17:03:20 +0700

    เพิ่ม svg PackageIcon

--- src/components/svg/index.tsx
+++ src/components/svg/index.tsx
@@ -110,3 +110,21 @@ export const magnify = (
         <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-4.35-4.35m0 0A7.5 7.5 0 1010.5 3a7.5 7.5 0 006.15 13.65z" />
     </svg>
 );
+
+export const PackageIcon = ({ className }: { className?: string }) => {
+    return (
+        <svg className={className || "w-6 h-6"}
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 24 24" 
+            fill="none" 
+            stroke="currentColor" 
+            strokeWidth="2" 
+            strokeLinecap="round" 
+            strokeLinejoin="round"
+        >
+            <path d="M21 16V8a2 2 0 0 0-1-1.73l-7-4a2 2 0 0 0-2 0l-7 4A2 2 0 0 0 3 8v8a2 2 0 0 0 1 1.73l7 4a2 2 0 0 0 2 0l7-4A2 2 0 0 0 21 16z"/>
+            <polyline points="3.27 6.96 12 12.01 20.73 6.96"/>
+            <line x1="12" y1="22.08" x2="12" y2="12"/>
+        </svg>
+    );
+};
