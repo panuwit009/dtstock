1c936c563c8a1490fc7f50050d1af84d0386aaec
ppp <rockzx007@gmail.com>
2025-11-24 00:09:34 +0700

    เพิ่ม CloseTab, QrCodeIcon, EditIcon, CharIcon, CalendarIcon

--- src/components/svg/index.tsx
+++ src/components/svg/index.tsx
@@ -137,3 +137,74 @@ export const PackageIcon = ({ className }: { className?: string }) => {
         </svg>
     );
 };
+
+export const CalendarIcon = ({ className }: { className?: string }) => {
+    return (
+        <svg className={className || "w-6 h-6"} fill="currentColor" viewBox="0 0 20 20">
+            <path fillRule="evenodd" d="M6 2a1 1 0 00-1 1v1H4a2 2 0 00-2 2v10a2 2 0 002 2h12a2 2 0 002-2V6a2 2 0 00-2-2h-1V3a1 1 0 10-2 0v1H7V3a1 1 0 00-1-1zm0 5a1 1 0 000 2h8a1 1 0 100-2H6z" clipRule="evenodd" />
+        </svg>
+    );
+};
+
+export const ChartIcon = ({ className }: { className?: string }) => {
+    return (
+        <svg 
+            className={className || "w-6 h-6"}
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 24 24" 
+            fill="none" 
+            stroke="currentColor" 
+            strokeWidth="2" 
+            strokeLinecap="round" 
+            strokeLinejoin="round"
+        >
+            <line x1="6" y1="20" x2="6" y2="10"/>
+            <line x1="12" y1="20" x2="12" y2="4"/>
+            <line x1="18" y1="20" x2="18" y2="14"/>
+        </svg>
+    );
+};
+
+export const EditIcon = ({ className }: { className?: string }) => {
+    return (
+        <svg className={className || "w-5 h-5 text-white"} 
+            fill="none" stroke="currentColor" viewBox="0 0 24 24">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} 
+                d="M11 5H6a2 2 0 00-2 2v11a2 2 0 002 2h11a2 2 0 002-2v-5m-1.414-9.414a2 2 0 112.828 2.828L11.828 15H9v-2.828l8.586-8.586z" />
+        </svg>
+    );
+}
+
+export const QrCodeIcon = ({ className }: { className?: string }) => {
+    return (
+        <svg 
+            className={className || "w-6 h-6 text-gray-900/50"}
+            xmlns="http://www.w3.org/2000/svg" 
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+        >
+            <rect x="3" y="3" width="5" height="5"/>
+            <rect x="16" y="3" width="5" height="5"/>
+            <rect x="3" y="16" width="5" height="5"/>
+            <path d="M21 16h-3a2 2 0 0 0-2 2v3"/>
+            <path d="M21 21v.01"/>
+            <path d="M12 7v3a2 2 0 0 1-2 2H7"/>
+            <path d="M3 12h.01"/>
+            <path d="M12 3h.01"/>
+            <path d="M12 16v.01"/>
+            <path d="M16 12h1"/>
+        </svg>
+    );
+};
+
+export const CloseTab = ({ className }: { className?: string; }) => {
+    return (
+        <svg className={className || "w-6 h-6"} fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
+        </svg>
+    )
+}
