1773809d5ffe2f983e34b5c7ff6535fd8d94d442
panuwit009 <tinsologuitar@gmail.com>
2025-11-24 11:52:46 +0700

    เอา svg ที่ไม่ใช้ออก

--- src/components/svg/index.tsx
+++ src/components/svg/index.tsx
@@ -20,70 +20,6 @@ className="w-8 h-8 fill-[#73ACF6]">
     24 24v32c0 13.3-10.7 24-24 24H136.6l120.5 114.8c9.8 9.3 10 24.8.4 34.3z"/>
 </svg> );
 
-export const Dashboard = ( { className }: { className?: string;}) => { 
-    return (
-    <svg className={className} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-        <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
-        <path d="M12.5 0c-.157 0-.31
-        1.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
-    </svg>
-    );
-
-};
-
-export const Kanban = ( { className }: { className?: string;}) => {
-    return (
-    <svg className={className} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 18">
-        <path d="M6.143 0H1.857A1.857 1.857 0 0 0 0 1.857v4.286C0 7.169.831 8 1.857 8h4.286A1.857 1.857 0 0 0 8 6.143V1.857A1.857 1.857 0 0 0 6.143 0Zm10 0h-4.286A1.857 1.857 0 0 0 10 1.857v4.286C10 7.169 10.831 8 11.857 8h4.286A1.857 1.857 0 0 0 18 6.143V1.857A1.857 1.857 0 0 0 16.143 0Zm-10 10H1.857A1.857 1.857 0 0 0 0 11.857v4.286C0 17.169.831 18 1.857 18h4.286A1.857 1.857 0 0 0 8 16.143v-4.286A1.857 1.857 0 0 0 6.143 10Zm10 0h-4.286A1.857 1.857 0 0 0 10 11.857v4.286c0 1.026.831 1.857 1.857 1.857h4.286A1.857 1.857 0 0 0 18 16.143v-4.286A1.857 1.857 0 0 0 16.143 10Z"/>
-    </svg>
-    );
-};
-
-export const Inbox = ( { className }: { className?: string;}) => {
-    return (
-    <svg className={className} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-        <path d="m17.418 3.623-.018-.008a6.713 6.713 0 0 0-2.4-.569V2h1a1 1 0 1 0 0-2h-2a1 1 0 0 0-1 1v2H9.89A6.977 6.977 0 0 1 12 8v5h-2V8A5 5 0 1 0 0 8v6a1 1 0 0 0 1 1h8v4a1 1 0 0 0 1 1h2a1 1 0 0 0 1-1v-4h6a1 1 0 0 0 1-1V8a5 5 0 0 0-2.582-4.377ZM6 12H4a1 1 0 0 1 0-2h2a1 1 0 0 1 0 2Z"/>
-    </svg>
-    );
-
-};
-
-export const Users = ( { className }: { className?: string;}) => {
-    return (
-    <svg className={className} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-        <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
-    </svg>
-    );
-
-};
-
-export const Products = ( { className }: { className?: string;}) => {
-    return (
-    <svg className={className} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 18 20">
-        <path d="M17 5.923A1 1 0 0 0 16 5h-3V4a4 4 0 1 0-8 0v1H2a1 1 0 0 0-1 .923L.086 17.846A2 2 0 0 0 2.08 20h13.84a2 2 0 0 0 1.994-2.153L17 5.923ZM7 9a1 1 0 0 1-2 0V7h2v2Zm0-5a2 2 0 1 1 4 0v1H7V4Zm6 5a1 1 0 1 1-2 0V7h2v2Z"/>
-    </svg>
-    );
-
-};
-
-export const SignIn = ( { className }: { className?: string;}) => {
-    return (
-    <svg className={className} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">\
-        <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
-    </svg>
-    );
-};
-
-export const SignUp = ( { className }: { className?: string;}) => {
-    return (
-    <svg className={className} aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-        <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.96 2.96 0 0 0 .13 5H5Z"/>
-        <path d="M6.737 11.061a2.961 2.961 0 0 1 .81-1.515l6.117-6.116A4.839 4.839 0 0 1 16 2.141V2a1.97 1.97 0 0 0-1.933-2H7v5a2 2 0 0 1-2 2H0v11a1.969 1.969 0 0 0 1.933 2h12.134A1.97 1.97 0 0 0 16 18v-3.093l-1.546 1.546c-.413.413-.94.695-1.513.81l-3.4.679a2.947 2.947 0 0 1-1.85-.227 2.96 2.96 0 0 1-1.635-3.257l.681-3.397Z"/>
-        <path d="M8.961 16a.93.93 0 0 0 .189-.019l3.4-.679a.961.961 0 0 0 .49-.263l6.118-6.117a2.884 2.884 0 0 0-4.079-4.078l-6.117 6.117a.96.96 0 0 0-.263.491l-.679 3.4A.961.961 0 0 0 8.961 16Zm7.477-9.8a.958.958 0 0 1 .68-.281.961.961 0 0 1 .682 1.644l-.315.315-1.36-1.36.313-.318Zm-5.911 5.911 4.236-4.236 1.359 1.359-4.236 4.237-1.7.339.341-1.699Z"/>
-    </svg>
-    )
-};
-
 export const Circle = ( { className }: { className?: string;}) => {
     return (
     <svg className={className} fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
@@ -93,7 +29,7 @@ export const Circle = ( { className }: { className?: string;}) => {
 };
 
 export const logoutIcon = (
-<svg fill="none" height="24" viewBox="0 0 24 24" width="24"xmlns="http://www.w3.org/2000/svg">
+<svg fill="none" height="24" viewBox="0 0 20 24" width="24"xmlns="http://www.w3.org/2000/svg">
     <path d="M17 16L21 12M21 12L17 8M21 12L7 12M13 16V17C13 18.6569 11.6569 20 10 20H6C4.34315
     20 3 18.6569 3 17V7C3 5.34315 4.34315 4 6 4H10C11.6569 4 13 5.34315 13 7V8"
     stroke="#ffffffff" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"/>
@@ -111,15 +47,20 @@ export const magnify = (
     </svg>
 );
 
-export const info = (
+export const Info = ({ className }: { className?: string }) => {
+    return (
     <svg
+        className={className}
         xmlns="http://www.w3.org/2000/svg"
-        x="0px" y="0px" width="34" height="34"
+        fill="currentColor"
+        x="0px" y="0px"
         viewBox="0 0 128 128"
     >
         <path d="M 64 6 C 32 6 6 32 6 64 C 6 96 32 122 64 122 C 96 122 122 96 122 64 C 122 32 96 6 64 6 z M 64 12 C 92.7 12 116 35.3 116 64 C 116 92.7 92.7 116 64 116 C 35.3 116 12 92.7 12 64 C 12 35.3 35.3 12 64 12 z M 64 29 C 58.5 29 54 33.5 54 39 C 54 44.5 58.5 49 64 49 C 69.5 49 74 44.5 74 39 C 74 33.5 69.5 29 64 29 z M 64 35 C 66.2 35 68 36.8 68 39 C 68 41.2 66.2 43 64 43 C 61.8 43 60 41.2 60 39 C 60 36.8 61.8 35 64 35 z M 64 58 C 58.5 58 54 62.5 54 68 L 54 92 C 54 97.5 58.5 102 64 102 C 69.5 102 74 97.5 74 92 L 74 68 C 74 62.5 69.5 58 64 58 z M 64 64 C 66.2 64 68 65.8 68 68 L 68 92 C 68 94.2 66.2 96 64 96 C 61.8 96 60 94.2 60 92 L 60 68 C 60 65.8 61.8 64 64 64 z"></path>
     </svg>
-);
+    );
+};
+
 export const PackageIcon = ({ className }: { className?: string }) => {
     return (
         <svg className={className || "w-6 h-6"}
@@ -207,4 +148,20 @@ export const CloseTab = ({ className }: { className?: string; }) => {
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M6 18L18 6M6 6l12 12" />
         </svg>
     )
+}
+
+export const Camera = () => {
+    return (
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 28" className="w-8 h-8">
+            <path d="M4,10a2,2,0,1,0,2,2A2,2,0,0,0,4,10Zm0,3a1,1,0,1,1,1-1A1,1,0,0,1,4,13Zm8.5-3A5.5,5.5,0,1,0,18,15.5,5.51,5.51,0,0,0,12.5,10Zm0,10A4.5,4.5,0,1,1,17,15.5,4.5,4.5,0,0,1,12.5,20Zm10-12H22V7.5A1.5,1.5,0,0,0,20.5,6h-1A1.5,1.5,0,0,0,18,7.5V8h-.78a.5.5,0,0,1-.47-.34L16.2,6a1.5,1.5,0,0,0-1.42-1H10.22A1.5,1.5,0,0,0,8.8,6L8.25,7.66A.5.5,0,0,1,7.78,8H7V7.5A1.5,1.5,0,0,0,5.5,6h-2A1.5,1.5,0,0,0,2,7.5V8H1.5A1.5,1.5,0,0,0,0,9.5v12A1.5,1.5,0,0,0,1.5,23h21A1.5,1.5,0,0,0,24,21.5V9.5A1.5,1.5,0,0,0,22.5,8ZM19,7.5a.5.5,0,0,1,.5-.5h1a.5.5,0,0,1,.5.5V8H19ZM3,7.5A.5.5,0,0,1,3.5,7h2a.5.5,0,0,1,.5.5V8H3Zm20,14a.5.5,0,0,1-.5.5H1.5a.5.5,0,0,1-.5-.5V9.5A.5.5,0,0,1,1.5,9H7.78A1.5,1.5,0,0,0,9.2,8l.54-1.63A.5.5,0,0,1,10.22,6h4.56a.5.5,0,0,1,.47.34L15.8,8a1.5,1.5,0,0,0,1.42,1H22.5a.5.5,0,0,1,.5.5ZM11.5,8h2a.5.5,0,0,0,0-1h-2a.5.5,0,0,0,0,1Zm1,4A3.5,3.5,0,1,0,16,15.5,3.5,3.5,0,0,0,12.5,12Zm0,6A2.5,2.5,0,1,1,15,15.5,2.5,2.5,0,0,1,12.5,18Zm7-8a.5.5,0,1,0,.5.5A.5.5,0,0,0,19.5,10Z"></path>
+        </svg>
+    );
+};
+
+export const Plus = () => {
+    return (
+        <svg className="w-8 h-8 text-gray-800 dark:text-white" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M5 12h14m-7 7V5"/>
+        </svg>
+    );
 }
