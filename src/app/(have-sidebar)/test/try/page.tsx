07c4c58978bbe5ace0e7b8912bdc3025d32699b3
panuwit009 <tinsologuitar@gmail.com>
2025-11-27 20:41:24 +0700

    ติดตั้ง react-moveable

--- src/app/(have-sidebar)/test/try/page.tsx
+++ src/app/(have-sidebar)/test/try/page.tsx
@@ -1,5 +1,36 @@
+"use client";
+import Moveable, { OnDrag } from "react-moveable";
+
 export default function Try () {
     return (
-        <div className="bg-red-200 text-5xl">Test</div>
+        <div className="bg-red-200 text-5xl" id="test">
+            Test
+            <Moveable 
+                target={document.getElementById("test")}
+                container={null}
+                origin={true}
+
+                /* Resize event edges */
+                edge={false}
+
+                /* draggable */
+                draggable={true}
+                onDrag={({
+                target,
+                beforeDelta, beforeDist,
+                left, top,
+                right, bottom,
+                delta, dist,
+                transform,
+                clientX, clientY,
+                }: OnDrag) => {
+                    console.log("onDrag left, top", left, top);
+                    // target!.style.left = `${left}px`;
+                    // target!.style.top = `${top}px`;
+                    console.log("onDrag translate", dist);
+                    target!.style.transform = transform;
+                }}
+            />
+        </div>
     )
 }
