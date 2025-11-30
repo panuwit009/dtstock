20476ddda4a5e024c2ae79f23ea8fe25f859b981
panuwit009 <tinsologuitar@gmail.com>
2025-11-30 16:04:39 +0700

    ทำ layout ui มือถือ

--- src/app/(have-sidebar)/home/page.tsx
+++ src/app/(have-sidebar)/home/page.tsx
@@ -12,7 +12,30 @@ export default function Home () {
 function MobileView () {
     return (
         <>
-            หน้าจอมือถือ
+            <div className="bg-blue-200">
+                <p className="text-4xl">asdasd</p>
+                <p className="text-4xl">asdasd</p>
+                <p className="text-4xl">asdasd</p>
+                <p className="text-4xl">asdasd</p>
+                <p className="text-4xl">asdasd</p>
+                <p className="text-4xl">asdasd</p>
+                <p className="text-4xl">asdasd</p>
+                <p className="text-4xl">asdasd</p>
+                <p className="text-4xl">asdasd</p>
+                <p className="text-4xl">asdasd</p>
+                <p className="text-4xl">asdasd</p>
+                <p className="text-4xl">asdasd</p>
+                <p className="text-4xl">asdasd</p>
+                <p className="text-4xl">asdasd</p>
+                <p className="text-4xl">asdasd</p>
+                <p className="text-4xl">asdasd</p>
+            </div>
+            <nav
+                className="absolute bottom-0 left-0 right-0 h-20
+                flex justify-center items-center bg-green-100"
+            >
+                asd
+            </nav>
         </>
     );
 }
