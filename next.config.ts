250f110191e19c2f07599be3f41fd8a826b62b55
panuwit009 <tinsologuitar@gmail.com>
2025-12-28 13:15:49 +0700

    config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  // output: 'export', basePath: '/dtstock', assetPrefix: '/dtstock',
 };
 
 export default nextConfig;
